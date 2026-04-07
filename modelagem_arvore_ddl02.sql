48f012b33e1e887cf64e7e0c5e3a921d822554c8
luiza_silva <luiza.santiagolt4@gmail.com>
2026-04-07 19:31:51 -0300

    Refactor categorias table structure and remove DB creationn
    Removed database creation and selection statements. Modified the 'categorias' table to include 'descricao_categoria' and 'categorias_id_categoria' with updated constraints.

--- modelagem_arvore_ddl02.sql
+++ modelagem_arvore_ddl02.sql
@@ -1,13 +1,12 @@
-CREATE DATABASE IF NOT EXISTS gestao_arvore_mercadologica;
-USE gestao_arvore_mercadologica;
-
--- Tabela Principal: Árvore Mercadológica
 CREATE TABLE categorias (
-    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    id_categoria INT NOT NULL AUTO_INCREMENT,
     nome_categoria VARCHAR(100) NOT NULL,
-    nivel ENUM('Departamento', 'Categoria', 'Subcategoria'), -- Ajuda a organizar a árvore
-    id_pai INT, -- Campo que cria a hierarquia
-    CONSTRAINT fk_no_pai FOREIGN KEY (id_pai) REFERENCES categorias(id_categoria)
+    descricao_categoria VARCHAR(1000),
+    categorias_id_categoria INT,
+    PRIMARY KEY (id_categoria),
+    CONSTRAINT fk_categoria_pai
+        FOREIGN KEY (categorias_id_categoria)
+        REFERENCES categorias (id_categoria)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
 );
-
-select * from categorias;
